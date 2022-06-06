75a984894fb3a24887985a515cf5b16ce13c4d27
gökhan özdemir <gokhannozzdemir@hotmail.com>
2022-06-07 00:40:14 +0300

    Commit

--- src/test/java/Localhost.java
+++ src/test/java/Localhost.java
@@ -37,4 +37,26 @@ public class Localhost {
                 statusCode(201).
                 log().all();
     }
+    @Test// Last situation what inserted new db info
+    public void localhost_3(){
+        baseURI = "http://localhost:3000";
+
+        given().
+                get("/users").
+                then().statusCode(200).
+                log().all();
+    }
+    @Test //Assert using with GET Method
+    public void localhost_4(){
+        baseURI = "http://localhost:3000";
+
+        given().
+                get("/subjects/").
+                then().
+                body("name[1]",equalTo("racer")).
+                statusCode(200).
+                log().all();
+
+    }
+
 }
